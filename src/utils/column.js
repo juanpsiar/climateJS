d262516bfab9f4119ad385a33b7e4ebd550fe8d2
unknown <chirlychplin@gmail.com>
2021-09-07 19:14:38 -0400

    column left finish

---
+++ src/utils/column.js
@@ -0,0 +1,12 @@
+let $search = document.querySelector('#search');
+let $modal = document.querySelector('#modal');
+let $close = $modal.querySelector('.search__close');
+
+$search.addEventListener('click', ()=>{
+    $modal.classList.remove('loaderClose');
+    $modal.classList.add('loader');
+})
+$close.addEventListener('click', ()=>{
+    $modal.classList.remove('loader');
+    $modal.classList.add('loaderClose');
+})
---
+++ src/utils/location.js
@@ -0,0 +1,28 @@
+let coordinates;
+
+const $gps = document.querySelector('#gps');
+
+const locationUser = navigator.geolocation;
+
+$gps.addEventListener('click', getLocation);
+
+//obtenemos la localizacion actual del usuario
+function getLocation(){
+  if (locationUser) {
+    locationUser.getCurrentPosition(showPosition);
+  } else {
+    return 'Geolocation is not supported by this browser.';
+  }
+};
+
+function setData(dataCoord){
+  coordinates = dataCoord;
+};
+
+function showPosition(position){
+  let cords = {
+    latitude: position.coords.latitude,
+    longitude: position.coords.longitude,
+  };
+  setData(cords);
+};
